8a3be35b18f2417fe4234f72697a21e8d4d13b00
Andres Bravo <bravoandres1706@gmail.com>
2025-05-10 13:29:22 -0400

    conexion a la base de datos en .env

--- src/db/conexion.ts
+++ src/db/conexion.ts
@@ -1,9 +1,20 @@
 import { Sequelize } from "sequelize";
 
+const db_config = {
+    nombre: process.env.NOMBRE_DB as string,
+    usuario: process.env.USUARIO_DB as string,
+    password: process.env.PASSWORD_DB as string,
+    host: process.env.PORT_DATABASE as string,
+}
 // Option 3: Passing parameters separately (other dialects)
-const sequelize = new Sequelize("juegos", "root", "", {
-    host: "localhost",
-    dialect: "mysql",
-});
+const sequelize = new Sequelize(
+    db_config.nombre,
+    db_config.usuario,
+    db_config.password,
+    {
+        host: db_config.host,
+        dialect: "mysql",
+    }
+);
 
 export default sequelize
