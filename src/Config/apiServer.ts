7c94bddea602f4ab6edab76a40af7ccf886a7d1c
Andres Bravo <bravoandres1706@gmail.com>
2025-12-31 11:14:11 -0400

    applicating cors

--- src/Config/apiServer.ts
+++ src/Config/apiServer.ts
@@ -1,71 +1,76 @@
-import * as dotenv from 'dotenv';
+import * as dotenv from "dotenv";
 dotenv.config(); // ¡Esto carga el archivo .env!
-import express, { Application, Request, Response } from 'express';
-import cors from 'cors';
-import UsuariosRoutes from '../Routes/usuariosRoutes';
-import PruebasRoutes from '../Routes/pruebasRoutes';
-import JuegosRoutes from '../Routes/juegosRoutes'
-import avataresRoutes from '../Routes/avataresRoutes';
+import express, { Application, Request, Response, NextFunction } from "express";
+import cors from "cors";
+import UsuariosRoutes from "../Routes/usuariosRoutes";
+import PruebasRoutes from "../Routes/pruebasRoutes";
+import JuegosRoutes from "../Routes/juegosRoutes";
+import avataresRoutes from "../Routes/avataresRoutes";
 
-import morgan from 'morgan';
+import morgan from "morgan";
 // actualizado
 
-import db from '../db/conexion'
+import db from "../db/conexion";
 
 // aqui es la base de datos y rutas
 
 class ApiServer {
-  private usuariosPath: string;
-  private app: Application;
-  private pruebasPath:string;
-  private juegosPath:string;
-  private avataresPath:string;
-  
+    private usuariosPath: string;
+    private app: Application;
+    private pruebasPath: string;
+    private juegosPath: string;
+    private avataresPath: string;
 
-  constructor() {
-    this.app = express();
-    this.usuariosPath = "/usuarios";
-    this.pruebasPath = "/pruebas";
-    this.juegosPath = "/juegos";
-    this.avataresPath = "/avatares";
-    this.middlewares();  // Llama a la función middleware
-    this.routes();       // Registra las rutas
-    this.dbConnet(); //conexion a la base de datos
-    
-  }
-
-  private middlewares(): void {
-    this.app.use(cors());
-    this.app.use(express.json());
-    this.app.use(morgan('dev'))
-    // this.app.use(express.static('public'));
-  }
-
-  public routes(): void {
-    this.app.use(this.usuariosPath, UsuariosRoutes);
-    this.app.use(this.pruebasPath, PruebasRoutes);
-    this.app.use(this.juegosPath,JuegosRoutes)
-    this.app.use(this.avataresPath,avataresRoutes)
-  }
-
-  public escuchar(): void {
-    const port = process.env.PORT || 4000;
-    this.app.listen(port, () => {
-      console.log(`API REST iniciada en el puerto ${port}`);
-    });
-  }
-
-  async dbConnet() {
-    //conexion a la base de datos
-
-    try {
-      await db.authenticate();
-      console.log("base de datos conectada");
-    } catch (error) {
-      console.log(error);
-      console.log('error al conectarse en la base de datos');
+    constructor() {
+        this.app = express();
+        this.usuariosPath = "/usuarios";
+        this.pruebasPath = "/pruebas";
+        this.juegosPath = "/juegos";
+        this.avataresPath = "/avatares";
+        this.middlewares(); // Llama a la función middleware
+        this.routes(); // Registra las rutas
+        this.dbConnet(); //conexion a la base de datos
     }
-  }
+
+    private middlewares(): void {
+        this.app.use(cors());
+        // this.app.use(
+        //     cors({
+        //         origin: true, // Refleja el origin que venga
+        //         credentials: false,
+        //     })
+        // );
+
+        this.app.use(express.json());
+        this.app.use(morgan("dev"));
+        // this.app.use(express.static('public'));
+    }
+
+    public routes(): void {
+        this.app.use(this.usuariosPath, UsuariosRoutes);
+        this.app.use(this.pruebasPath, PruebasRoutes);
+        this.app.use(this.juegosPath, JuegosRoutes);
+        this.app.use(this.avataresPath, avataresRoutes);
+    }
+
+    public escuchar(): void {
+        const port = process.env.PORT || 4000;
+        this.app.listen(port, () => {
+            console.log(`API REST iniciada en el puerto ${port}`);
+        });
+    }
+
+    async dbConnet() {
+        //conexion a la base de datos
+
+        try {
+            await db.authenticate();
+            console.log("base de datos conectada");
+        } catch (error) {
+            console.log(error);
+            console.log("error al conectarse en la base de datos");
+        }
+    }
 }
 
 export default ApiServer;
